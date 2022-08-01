1375f3149575cd16996a9dc68618a6d93a621616
Alex <alexeym75@gmail.com>
2022-08-01 18:50:10 +0700

    Tasks 1,2,3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,76 @@
 public class Main {
-    public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+    public static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
+    }
+
+    public static void printOSVersion(byte clientOS, int clientDeviceYear) {
+        if (clientOS == 0) {
+            if (clientDeviceYear >= 2015) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        } else if (clientOS == 1) {
+            if (clientDeviceYear >= 2015) {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        } else {
+            System.out.println("Некорректное значение clientOS: " + clientOS);
+        }
+    }
+
+    public static int calcDeliveryDays(int distance) {
+        int deliveryDays = 1;
+
+        if (distance > 20) {
+            deliveryDays++;
+        }
+        if (distance > 60) {
+            deliveryDays++;
+        }
+        if (distance > 100) {
+            deliveryDays = 0;
+        }
+        return deliveryDays;
+    }
+
+    // Метод для задачи: homeworkConditionals -> Task3
+    public static void Task1() {
+        int year = 40;
+
+        if (isLeapYear(year)) {
+            System.out.println(year + " - високосный год");
+        } else {
+            System.out.println(year + " - не високосный год");
+        }
+    }
+
+    // Метод для задачи: homeworkConditionals -> Task2
+    public static void Task2() {
+        byte clientOS = 4; // 0 — iOS, 1 — Android
+        int clientDeviceYear = 2018;
+
+        printOSVersion(clientOS, clientDeviceYear);
+    }
+
+    // Метод для задачи: homeworkConditionals -> Task4
+    public static void Task3() {
+        int deliveryDistance = 750;
+        int deliveryDays = calcDeliveryDays(deliveryDistance);
+
+        if (deliveryDays > 0) {
+            System.out.println("Потребуется дней: " + deliveryDays);
+        } else {
+            System.out.println("Доставка в ваш регион недоступна");
+        }
+    }
+
+    public static void main(String[] args) {
+        Task1();
+        Task2();
+        Task3();
     }
 }
